bf56045d5da868c3be8fa8e90d77aa8e55012702
Zelada Adrian <zelada_torrez@hotmail.com>
2014-12-02 02:24:42 -0400

    notification

---
+++ ServerNode/users.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Zelada_Torrez on 01-12-14.
+ */
+module.exports=function(){
+
+    var Cod_user="";
+    var Image="";
+    var Nombre="";
+    var ApellidoPa="";
+    var ApellidoMa="";
+
+}
